b602b670ecb06c3bd4201f10a7eee52ff7a17d75
Aleksey <a.y.zayitsev@gmail.com>
2023-01-17 00:07:42 +0300

    Ввести с клавиатуры числа и выяснить сколько чисел больше нуля

---
+++ Task_1/Program.cs
@@ -0,0 +1,30 @@
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+int sum = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"Введите {i + 1} число: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    array[i] = n;
+
+}
+Console.Write("[ ");
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]} ");
+}
+Console.Write("] - Ваш массив чисел.");
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        sum++;
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"Количество чисел больше нуля = {sum}.");
